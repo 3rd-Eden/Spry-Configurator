62a77a6950abfa1ade1911b70f9a9173dcf0c54e
Arnout Kazemier <info@3rd-Eden.com>
2010-06-01 22:08:07 +0200

    Made it working.

--- main.py
+++ main.py
@@ -17,16 +17,43 @@
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util
 
+class js(webapp.RequestHandler):
+    def get(self, request):
+        self.response.out.write('Hello world!')
+        
+        # We are trying to be as tolerant as possible
+        # Remove double slashes to prevent pointless array
+        # creating when we are converting the request
+        while request[-1] == "/":
+        	request = request[:len(request)-1]
+        
+        # Users do not need to prefix the files with Spry and
+        # we lowercase everything as the filenames are lowercase
+        # as well. Better one big .lower() than many .lower()
+        query = request.lower().replace('spry','').split('/')
+        	
+        self.response.out.write( query )
+
+class css(webapp.RequestHandler):
+    def get(self, request):
+        self.response.out.write('Hello world!')
+
+class beacon(webapp.RequestHandler):
+    def get(self):
+        self.response.out.write('Hello world!')
 
 class MainHandler(webapp.RequestHandler):
     def get(self):
         self.response.out.write('Hello world!')
 
-
 def main():
-    application = webapp.WSGIApplication([('/', MainHandler)],
-                                         debug=True)
-    util.run_wsgi_app(application)
+    interface = webapp.WSGIApplication(
+    	[
+    		(r'/js/(.*)/$', js),
+    		(r'/css/(.*)/$', css),
+    		('/', MainHandler)
+    	], debug=True)
+    util.run_wsgi_app(interface)
 
 
 if __name__ == '__main__':
