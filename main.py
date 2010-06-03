44f673bc2d9f05e428094736781ad07193356b44
Arnout Kazemier <info@3rd-Eden.com>
2010-06-04 00:14:25 +0200

    Updated with cache handling

--- main.py
+++ main.py
@@ -16,8 +16,18 @@
 #
 # @Author: Arnout Kazemier, <info@3rd-eden.com>
 
+# "native" python imports
+import hashlib
+import re
+import datetime
+
+# Google App Engine imports
+from google.appengine.api import memcache
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util
+
+# Custom imports
+from model.datastorage import ds_data
 
 class js(webapp.RequestHandler):
     def get(self, path):
@@ -30,6 +40,14 @@ class js(webapp.RequestHandler):
         if not request:
 			self.error(404)
 			return
+		
+		# handle 304 requests
+		# at a later stage we will allow the length to be specified in the url and parse it
+		# modifiedsince = datetime.datetime.strptime(self.request.headers.get('If-Modified-Since'), "%a, %d %b %Y %H: %M:%S GMT")
+		# for now everyrequest with a if-modified-since header will recieve a 304 as it passed our generation process
+        if self.request.headers.get('If-Modified-Since'):
+			self.error(304)
+			return
 			
         # We are trying to be as tolerant as possible
         # Remove double slashes to prevent pointless array
@@ -41,6 +59,22 @@ class js(webapp.RequestHandler):
         # we lowercase everything as the filenames are lowercase
         # as well. Better one big .lower() than many .lower()
         query = request.lower().replace('spry','').split('/')
+        
+        # check for memcache hits so we can serve a correct request
+        # and have a clean token to use.
+        token = ''.join(query) + "js"
+        cache = memcache.get(token)
+        
+        # caching headers
+        expires = datetime.datetime.now() + datetime.timedelta(days=365)
+        self.response.headers['Last-Modified'] = datetime.datetime.now().strftime("%a, %d %b %Y %H:%M:%S GMT")
+        self.response.headers["Cache-Control"] = "public, max-age=31536000"
+        self.response.headers["Expires"] = expires.strftime("%a, %d %b %Y %H:%M:%S GMT") 
+        
+        # Memory cache hit! \o/ return
+        if cache:
+        	self.response.out.write(cache)
+        	return
         	
         self.response.out.write( query )
 
